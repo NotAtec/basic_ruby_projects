050945547b4dc4286f3f85cff5e51e2252006eb6
Jeroen B <jeroenb04@live.com>
2022-01-04 13:52:33 +0100

    Added tests & fixed to spec
    Added required test calls,
    changed method to properly respond as to spec
    
    Slight change required: Keep spaces in string

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,9 +1,14 @@
 def substrings(string, dictionary)
   hash = {}
   string.gsub!(/\W/, '') #Clean up non word characters from string for parsing
+  string.downcase!
   dictionary.each do |check|
-    hash[check.to_sym] = string.scan(check).size
+    hash[check] = string.scan(check).size
   end
   hash.select! {|k, v| v != 0} #Clean up final hash
   hash
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
