13d7b40752c297ea24dc22106e06b8b26666a7d1
Jeroen B <jeroenb04@live.com>
2022-01-04 13:49:58 +0100

    Added clean-up
    Non word chars are removed from initial string
    Only non-0 values in hash are kept, as to spec.

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,7 +1,9 @@
 def substrings(string, dictionary)
   hash = {}
+  string.gsub!(/\W/, '') #Clean up non word characters from string for parsing
   dictionary.each do |check|
     hash[check.to_sym] = string.scan(check).size
   end
+  hash.select! {|k, v| v != 0} #Clean up final hash
   hash
 end
