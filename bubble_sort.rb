b19690d25d686d586fb8eb1852f9c2d91e61344f
Jeroen B <jeroenb04@live.com>
2022-01-05 13:03:44 +0100

    Tried out rubocop on bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,11 +1,11 @@
 def bubble_sort(array)
   unsorted = true
-  while unsorted do
+  while unsorted
     unsorted = false
     array.each_with_index do |val, idx|
-      if idx == array.size - 1
-        break
-      elsif val > array[idx + 1]
+      break if idx == array.size - 1
+
+      if val > array[idx + 1]
         unsorted = true
         array[idx], array[idx + 1] = array[idx + 1], array[idx]
       end
@@ -14,4 +14,4 @@ def bubble_sort(array)
   array
 end
 
-p bubble_sort([4,3,78,2,0,2]) #Expect: [0,2,2,3,4,78]
+p bubble_sort([4,3,78,2,0,2]) # Expect: [0,2,2,3,4,78]
